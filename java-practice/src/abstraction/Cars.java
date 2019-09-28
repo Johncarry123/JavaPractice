cd8ce8f4efcbc300a6895f7ce1fdf72dfce10401
John Carry <carryjohn071@gmail.com>
2019-09-28 11:46:42 +0100

    My third commit

--- java-practice/src/abstraction/Cars.java
+++ java-practice/src/abstraction/Cars.java
@@ -4,4 +4,12 @@ package abstraction;
  * Created by Jay Vaghani on 28-Sep-2019
  */
 public class Cars {
+
+    String name;
+    int speed;
+
+    public Cars(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
 }
